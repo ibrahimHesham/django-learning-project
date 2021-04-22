5ff48a1e29724255b458d6b1047843038fbde8e1
Ibrahim-Hesham <ibrahim.hesham.cs@gmail.com>
2021-04-22 15:15:22 +0200

    added isbn and categories tables and their raletions

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30) 
+
+    def __str__ (self):
+        return self.name
+
+class Isbn(models.Model):
+    # created_at = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=30, default="def description")
+
+    def __str__ (self):
+        return self.description
+
 class Books(models.Model):
     title = models.CharField(max_length=30)
-    author = models.CharField(max_length=30)
+    # author = models.CharField(max_length=30)
     price = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="Books", default=1)
+    categories = models.ManyToManyField(Category)
+    Isbn = models.OneToOneField(Isbn, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__ (self):
         return self.title
+
