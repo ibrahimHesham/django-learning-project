60e82845564c611258cc9ae10c88f3197d5c60b2
Ibrahim-Hesham <ibrahim.hesham.cs@gmail.com>
2021-04-24 04:37:04 +0200

    added middleware

--- bookstore/bookstore/settings.py
+++ bookstore/bookstore/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'books.middleware.SimpleMiddleware',
 ]
 
 ROOT_URLCONF = 'bookstore.urls'
